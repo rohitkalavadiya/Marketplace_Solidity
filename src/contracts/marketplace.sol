d7fa7b289305f96740c6c0393e6796975cdf5360
rohitkalavadiya <rohitkalavadiya9@gmail.com>
2022-03-30 17:05:17 +0530

    Marketplace code push with bug on test environment

---
+++ src/contracts/marketplace.sol
@@ -0,0 +1,75 @@
+pragma solidity >= 0.5.0 <0.9.0;
+
+contract Marketplace {
+    
+    string public name;
+    uint public product_count;
+    mapping (uint => Product) public products;
+
+    struct Product {
+        uint id;
+        string name;
+        uint price;
+        address payable owner;
+        bool purchased;
+    }
+    event ProductCreated(
+        uint id,
+        string name,
+        uint price,
+        address payable owner,
+        bool purchased
+    );
+    event ProductPurchase(
+        uint id,
+        string name,
+        uint price,
+        address payable owner,
+        bool purchased
+    );
+
+    constructor() public {
+        name = "Rohit Market place product";
+    }
+
+    function createProduct(string memory _name, uint _price ) public {
+        // make sure parameter is correct
+        require(bytes(_name).length > 0);
+        require(_price > 0);
+        // increment product count 
+        product_count ++;
+        //creat a product 
+        products[product_count] = Product(product_count,_name , _price, msg.sender , false);
+        //trigger a event 
+        emit ProductCreated(product_count, _name , _price, msg.sender , false);
+    }
+
+    function PurchaseProduct(uint _id) public payable {
+        //fetch product
+        Product memory _product = products[_id];
+        //featch owner
+        address  payable _seller = _product.owner;
+        //make sure product is valid
+        require(_product.id > 0  && _product.id <= product_count);
+        // product amount have a buyer have checked 
+        require(msg.value >= _product.price);
+        //check the product dones not sell
+        require(!_product.purchased);
+        //check the buyer is not seller 
+        require(_seller !=msg.sender);
+        // trasfer owneship to buyer    
+        _product.owner =  msg.sender;
+        //make sure product is valid
+        _product.purchased = true ;
+        //update the product
+        products[_id] = _product;
+        //pay ether to seller
+        address(_seller).transfer(msg.value);
+        //tigger event  
+        emit ProductPurchase(product_count, _product.name , _product.price, msg.sender , true);
+
+    }
+
+
+
+}
---
+++ test/Marketplace.test.js
@@ -0,0 +1,93 @@
+const Marketplace = artifacts.require('./Marketplace.sol')
+
+contract ('Marketplace',( [deployer , seller , buyer])=>{
+let marketplace
+
+    before (async() =>{
+
+     marketplace = await Marketplace.deployed()
+    })
+    describe('deployment', async()=>{
+         it('deploys succesfully', async()=>{
+            const address = await  marketplace.address
+            assert.notEqual(address, 0x0)
+            assert.notEqual(address, '')
+            assert.notEqual(address, null)
+            assert.notEqual(address, undefined)
+        })
+        it('has a name', async()=>{
+            const name = await marketplace.name();
+            assert.notEqual(name, 'Rohit Market place worked')
+        })
+    })
+
+   
+
+    describe('products', async()=>{
+        let result , product_count
+        before(async ()=>{
+            result = await marketplace.createProduct('samsung m30s', web3.utils.toWei('1','Ether'),{from:seller })
+            product_count = await marketplace.product_count()
+           }) 
+      it('creates products', async ()=>{
+        assert.equal(product_count,1)
+         // check the log
+       const event = result.log[0].args
+       assert.equal(event.id.toNumber(), product_count.toNumber(), ' id is correct')
+       assert.equal(event.name, 'samsung m30s ', ' name is correct')
+       assert.equal(event.price, '1000000000000000000' ,' Price is correct')
+       assert.equal(event.owner, seller , ' Owner is correct')
+       assert.equal(event.purchased, false ,  ' purchased is correct')
+
+
+      await await marketplace.createProduct('', web3.utils.toWei('1','Ether'),{from:seller }).shoud.be.rejected;
+      await await marketplace.createProduct('samsung m30s', 0 ,{from:seller }).shoud.be.rejected;
+
+       })
+
+      it('List products ', async ()=>{
+       const product = await marketplace.products(product_count)
+        assert.equal(product.id.toNumber(), product_count.toNumber(), ' id is correct')
+        assert.equal(product.name, 'samsung m30s ', ' name is correct')
+        assert.equal(product.price, '1000000000000000000' ,' Price is correct')
+        assert.equal(product.owner, seller , ' Owner is correct')
+        assert.equal(product.purchased, false ,  ' purchased is correct')
+      })
+
+      it('selles products ', async ()=>{
+        //treack seller balance before the purchase
+        let oldSellerBalance 
+        oldSellerBalance = await web3.eth.getBalance();
+        oldSellerBalance = new web3.utils.BN(oldSellerBalance)
+
+        result = await marketplace.purchaseProduct(product_count,{from: buyer , value: web3.utils.toWei('1','Ether')})
+        // check the log
+        const event = result.log[0].args
+       assert.equal(event.id.toNumber(), product_count.toNumber(), ' id is correct')
+       assert.equal(event.name, 'samsung m30s ', ' name is correct')
+       assert.equal(event.price, '1000000000000000000' ,' Price is correct')
+       assert.equal(event.owner, buyer , ' Owner is correct')
+       assert.equal(event.purchased, true ,  ' purchased is correct')
+
+       //check the fund
+       let newSellerBalance 
+       newSellerBalance = await web3.eth.getBalance();
+       newSellerBalance = new web3.utils.BN(newSellerBalance)
+
+       let price
+       price = web3.utils.toWei('1','Ether')
+       price = new web3.utils.BN(price)
+
+       const exepectedBalance = oldSellerBalance.add(price)
+
+       assert.equal(newSellerBalance.toString(), exepectedBalance.toString())
+
+        //faliure 
+        await marketplace.purchaseProduct(99, {from: buyer , value: web3.utils.toWei('1','Ether')}).shoud.be.rejected
+        await marketplace.purchaseProduct(product_count, {from: buyer , value: web3.utils.toWei('0.5','Ether')}).shoud.be.rejected
+        await marketplace.purchaseProduct(product_count, {from: deployer , value: web3.utils.toWei('1','Ether')}).shoud.be.rejected
+        await marketplace.purchaseProduct(product_count,  {from: buyer , value: web3.utils.toWei('1','Ether')}).shoud.be.rejected
+       })
+   })
+
+})
